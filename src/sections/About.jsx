944f1259177102540e31b3c32e00cecde975d495
Legaz <worldoflegaz@icloud.com>
2025-06-12 01:36:35 +0900

    finished project section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
         }, 2000);
     }
   return (
-    <section className="c-space my-20">
+    <section className="c-space my-20" id="about">
       <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
@@ -41,7 +41,7 @@ const About = () => {
             <div>
               <p className="grid-headtext">Tech Stack</p>
               <p className="grid-subtext">
-                I specialize in JavaScript/TypeScript with focun on React,Vue.jz
+                I specialize in JavaScript/TypeScript with focun on React,Vue.js
                 and Next.js ecosystem{" "}
               </p>
             </div>

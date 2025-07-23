e463e160f5e79894c9acdb3f69d1854a713e240c
Legaz <worldoflegaz@icloud.com>
2025-07-23 14:35:47 +0900

    fix to deploy

--- src/constants/index.js
+++ src/constants/index.js
@@ -58,6 +58,43 @@ export const clientReviews = [
 
 export const myProjects = [
   {
+    title: 'Travel Page',
+    desc: 'A responsive and interactive landing page built for the Bookmark app. The project demonstrates strong attention to UI/UX, component structure, and responsive design principles.',
+    subdesc:
+      'Crafted a professional business demo website, integrating visually appealing design, clean layout structure, and fully responsive behavior across all devices using modern web technologies',
+    href: 'https://legaz-demo-website.netlify.app',
+    texture: '/textures/project/project1.mp4',
+    logo: '/assets/project-logo1.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
     title: ' SaaS App Landing Page',
     desc: 'A responsive and interactive landing page built for the Bookmark app. The project demonstrates strong attention to UI/UX, component structure, and responsive design principles.',
     subdesc:
@@ -176,6 +213,43 @@ export const myProjects = [
     subdesc:
       'Developed a modern and fully responsive landing page for the Bookmark app, focusing on smooth user experience, interactive UI components, and clean, maintainable code using React and TailwindCSS.',
     href: 'https://profound-flan-6af45f.netlify.app',
+    texture: '/textures/project/project4.mp4',
+    logo: '/assets/project-logo4.png',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Rest Api Search Countries',
+    desc: 'A clean, modern, and fully responsive landing page developed for the Frontend Mentor Bookmark challenge. The page promotes a fictional bookmarking service with clear calls to action, feature descriptions, FAQs, and an email subscription form.',
+    subdesc:
+      'Developed a modern and fully responsive landing page for the Bookmark app, focusing on smooth user experience, interactive UI components, and clean, maintainable code using React and TailwindCSS.',
+    href: 'https://restap.netlify.app',
     texture: '/textures/project/project4.mp4',
     logo: '/assets/project-logo4.png',
     logoStyle: {
